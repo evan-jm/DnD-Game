fd600b61ddb658a192d0a5040816e18f30296bef
Evan Mitchell <ejm0214@gmail.com>
2024-06-25 10:48:55 -0400

    Making files

---
+++ src/Character.h
@@ -0,0 +1,18 @@
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+#import <iostream>
+
+class Character {
+private:
+  std::string name;
+  std::string description;
+  int strength;
+  int dexterity;
+  int constitution;
+  int intelligence;
+  int wisdom;
+  int charisma;
+};
+
+#endif
